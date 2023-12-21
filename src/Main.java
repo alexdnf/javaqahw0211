3d94e9dee19e01c11cc6b377c982cb377748f7f1
Alex Deyanov <deyanovan@yandex.ru>
2023-12-21 19:57:19 +0300

    ReformatCode

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-       int amount = 600; // начальная сумма
-       int deposit = 1500; // сумма пополнения
-       int cashback = 100; // на какую сумму будет начисляться 1 бонусный рубль
+        int amount = 600; // начальная сумма
+        int deposit = 1500; // сумма пополнения
+        int cashback = 100; // на какую сумму будет начисляться 1 бонусный рубль
 
-       int bonus;
-       if (deposit > 1000) {
-           bonus = deposit/cashback;
-       } else {
-           bonus = 0;
-       }
-       int total = amount + deposit + bonus; // итоговая сумма на счете после пополнения
+        int bonus;
+        if (deposit > 1000) {
+            bonus = deposit / cashback;
+        } else {
+            bonus = 0;
+        }
+        int total = amount + deposit + bonus; // итоговая сумма на счете после пополнения
 
         System.out.println(deposit + " Сумма пополнения");
         System.out.println(total + " Всего средств на счете");
